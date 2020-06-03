00fe68505ecfdf973de2c245a7425a069312eb7e
lozzoc <reach.maddaloc@gmail.com>
2020-06-02 23:05:22 -0700

    im trying to figure out what the data is like but its hard.

--- ArchiveScript.py
+++ ArchiveScript.py
@@ -6,5 +6,20 @@ p =Piazza()
 p.user_login()
 cs290 = p.network(CLASS_ID)
 posts = cs290.iter_all_posts(limit=1)
+count =0
 for post in posts:
-    print(post)
+    if(count>1):
+        break
+    for key,value in post.items():
+        print(key)
+    print(post["change_log"])
+    print("^^^^change_log^^^^^")
+    print(post["history"])
+    print("^^^^history^^^^^")
+    print(post["data"])
+    print("^^^^data^^^^^")
+    print(post["children"])
+    print("^^^^CHILDREN^^^^^")
+
+
+    count+=1
