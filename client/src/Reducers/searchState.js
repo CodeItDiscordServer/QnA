4c23e86ac8eccbd5e51e3a4b7f6de867ec43f5b7
Ashween Mankash <ashweenmankash@gmail.com>
2020-06-14 14:37:02 +0530

    Added Test and setup github actions

--- client/src/Reducers/searchState.js
+++ client/src/Reducers/searchState.js
@@ -1,7 +1,12 @@
+import {FETCHING,SET_SEARCH_RESULTS,SEARCH,TOGGLE_TAG} from '../ActionTypes/ActionTypes'
 const initialState = {
     searchResults : [],
     filters:{
         tags:tags_hardcoded
+    },
+
+    pageState:{
+        loading:false
     }
 }
 
@@ -18,6 +23,20 @@ const SEARCH_URL = "http://localhost:5000/api/search";
 
 const searchState = (state=initialState,action)=>{
     switch(action.type){    
+        case FETCHING:
+            let newState = Object.assign({},state,{pageState:{loading:true}})
+            return newState
+        
+        case SET_SEARCH_RESULTS:
+            if(action.status===404)
+                let newState = Object.assign({},state,{
+                    pageState:{loading:false,error:true,message:"No Search Results"},
+                })
+            else if(action.status ===200){
+                let newState = Object.assign({},state,{
+                    pageState:{loading:false,error:true,message:"No Search Results"},
+                })
+            }
         
         default:
             return state
---
+++ client/src/Reducers/tests/searchState.test.js
@@ -0,0 +1,5 @@
+describe("Testing test" ,()=>{
+    it("1+1",()=>{
+        expect(1+1).toBe(2)
+    })
+})
