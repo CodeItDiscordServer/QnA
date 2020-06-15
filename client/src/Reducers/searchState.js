b668858af07b1575a10f8f8cefec4ac4a37f5134
Ashween Mankash <ashweenmankash@gmail.com>
2020-06-15 23:34:10 +0530

    chore: Applied new switch cases to handle actions

--- client/src/Reducers/searchState.js
+++ client/src/Reducers/searchState.js
@@ -1,8 +1,9 @@
-import {FETCHING,SET_SEARCH_RESULTS,SEARCH,TOGGLE_TAG} from '../ActionTypes/ActionTypes'
+import {FETCHING,SET_SEARCH_RESULTS,SEARCH,TOGGLE_TAG,UPDATE_SEARCH_TAG} from '../ActionTypes/ActionTypes'
 
-
+const SEARCH_URL = "/api/search";
 
 const tags_hardcoded={
+
     "hw1": false,
     "hw2": false,
     "hw3": false,
@@ -12,6 +13,9 @@ const tags_hardcoded={
 const initialState = {
     searchResults : [],
     filters:{
+        searchText: "",
+        i_answer: false,
+        s_answer: false,
         tags:tags_hardcoded
     },
 
@@ -20,16 +24,33 @@ const initialState = {
     }
 }
 
+const pageState =  (state=initialState.pageState,action)=>{
+    switch(action.type){
+        case FETCHING:
+            let newState = Object.assign({},state,{loading:true})
+            return newState
+       
+        default:
+            return state;
+    }
+}
+
+const filterState  = (state=initialState.filters,action)=>{
+    switch(action.type){
+        case TOGGLE_TAG:
+            return Object.assign({},state,{tags:action.value})
+        
+        case UPDATE_SEARCH_TAG:
+                return Object.assign({},state,{searchText:action.value})
+        default:
+        return state;
+    }
+}
 
 
-const SEARCH_URL = "/api/search";
 
 const searchState = (state=initialState,action)=>{
     switch(action.type){
-        case FETCHING:
-            let newState = Object.assign({},state,{pageState:{loading:true}})
-            return newState
-
         case SET_SEARCH_RESULTS:
             if(action.status===204){
                 let newState = Object.assign({},state,{
@@ -44,11 +65,18 @@ const searchState = (state=initialState,action)=>{
             }
             else if(action.status===303){}
         default:
+            return {
+                searchresults:state.searchResults,
+                pageState:pageState(state.pageState,action),
+                filters:filterState(state.filterState,action)
+            }
             return state
     }
 }
 /* these will notwork, with the combine reducers, now everytihg has another layer.
 state.search.pageState.searchResults, but searchresuls is not even in pageState.
+
+Done. :)
 */
 export const isLoading = (state)=>state.pageState.isLoading
 export const searchResults = (state) => state.pageState.searchResults
