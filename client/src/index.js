235a836f7d923f71f454d98ffa493b657c7b75ad
Ashween Mankash <ashweenmankash@gmail.com>
2020-06-15 23:33:22 +0530

    Applied Thunk middle ware and added Redux chrome extenion hook

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,8 @@ ReactDOM.render(
     </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
+  
 );
 
 // If you want your app to work offline and load faster, you can change
