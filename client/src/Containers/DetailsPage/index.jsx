5670e8b8037c03f63718df568272cdfa1a179829
Ashween <ashweenmankash@gmail.com>
2020-11-10 22:29:41 +0530

    Removed traces of style.js for detailed View  because style for it are now encoded in global css

--- client/src/Containers/DetailsPage/index.jsx
+++ client/src/Containers/DetailsPage/index.jsx
@@ -1,21 +1,21 @@
 /**  @jsx jsx */
 /* @jsxFrag React.Fragment */
-import {  jsx } from '@emotion/core';
-import {Component} from 'react'
+import { jsx } from '@emotion/core';
+import { Component } from 'react'
 import { Divider } from '@material-ui/core'
 import { LinearProgress } from "@material-ui/core"
-import {Link} from "react-router-dom"
-import {connect} from 'react-redux';
-import {Button} from "@material-ui/core";
-
-import DetailsStyle from "./style.js"
-import {isSearchPageLoading,
-        getPiazzaDetails} from "../../Reducers/index.js"
-import {DetailsJsonSearchSequence} from  "../../Actions/searchActions.js"
+import { Link } from "react-router-dom"
+import { connect } from 'react-redux';
+import { Button } from "@material-ui/core";
+import {
+  isSearchPageLoading,
+  getPiazzaDetails
+} from "../../Reducers/index.js"
+import { DetailsJsonSearchSequence } from "../../Actions/searchActions.js"
 import ShareLinkAlert from "../../components/LinkToDetails.js"
 
 
-const stateToProps = state =>({
+const stateToProps = state => ({
   isLoading: isSearchPageLoading(state),
   details: getPiazzaDetails(state),
 })
@@ -30,56 +30,56 @@ const dispatchToProps = {
 
 class DetailsPage extends Component {
 
-  componentDidMount(){
-    if(this.props.ids.length && this.props.ids.length !== this.props.details.length){
+  componentDidMount() {
+    if (this.props.ids.length && this.props.ids.length !== this.props.details.length) {
       this.props.DetailsJsonSearchSequence(this.props.ids)
     }
   }
 
-  renderDetails(){
-    return (<div css={DetailsStyle}>
-      {this.props.details.length > 0 && this.props.details.map(function(post,index){
+  renderDetails() {
+    return (<div>
+      {this.props.details.length > 0 && this.props.details.map(function (post, index) {
         return (
           <div key={`post-#-${index}`} className="post-card">
-              {
-                post.tags.map(function(tag,tagI){
-                  return (
-                    <span key={`post-${index}-tag-${tagI}`}
-                          className="details-tag">
-                      {tag}
-                    </span>
-                  )
-                })
-              }
-              <p className="details-question-subject">{post.post.subject}</p>
-              <p className="details-question-content">{post.post.content}</p>
+            {
+              post.tags.map(function (tag, tagI) {
+                return (
+                  <span key={`post-${index}-tag-${tagI}`}
+                    className="details-tag">
+                    {tag}
+                  </span>
+                )
+              })
+            }
+            <p className="details-question-subject">{post.post.subject}</p>
+            <p className="details-question-content">{post.post.content}</p>
 
-              <h6>Replies</h6>
-              {
-                post.replies.map(function(reply,replyI){
-                  return (
+            <h6>Replies</h6>
+            {
+              post.replies.map(function (reply, replyI) {
+                return (
                   <div key={`post-${index}-reply-${replyI}`}
-                      className="details-reply">
+                    className="details-reply">
                     <p>{reply.reply}</p>
                     {reply.followups.length > 0 && (<h6>Followups</h6>)}
                     {
-                        reply.followups.length > 0 && reply.followups.map(function(followup,follI){
-                              return (
-                                <p key={`post-${index}-reply-${replyI}-followup-${follI}`}
-                                  className="details-followup">
-                                  {followup.reply}
-                                </p>
-                              )
+                      reply.followups.length > 0 && reply.followups.map(function (followup, follI) {
+                        return (
+                          <p key={`post-${index}-reply-${replyI}-followup-${follI}`}
+                            className="details-followup">
+                            {followup.reply}
+                          </p>
+                        )
                       })
                     }
                   </div>
-                  )
-                })
-              }
+                )
+              })
+            }
 
 
 
-              <hr />
+            <hr />
 
           </div>
         )
@@ -89,27 +89,27 @@ class DetailsPage extends Component {
 
   }
 
-  render(){
-    let {isLoading,ids} = this.props;
+  render() {
+    let { isLoading, ids } = this.props;
     return (
-        <div className="DetailsListContainer">
+      <div className="DetailsListContainer">
         {isLoading && (<LinearProgress />)}
 
 
         <Button type="button">
-          <Link to="/" style={{zIndex: 5}}>
+          <Link to="/" style={{ zIndex: 5 }}>
             Back To Search
             </Link>
         </Button>
-        <ShareLinkAlert ids={ids}  />
-          <Divider variant="middle"/>
-          {/* We have a context Provider
+        <ShareLinkAlert ids={ids} />
+        <Divider variant="middle" />
+        {/* We have a context Provider
             to hold the local state of the each active post.*/}
-          {!isLoading && this.renderDetails()}
+        {!isLoading && this.renderDetails()}
 
-        </div>
-      )
+      </div>
+    )
   }
 }
 
-export default connect(stateToProps,dispatchToProps)(DetailsPage);
+export default connect(stateToProps, dispatchToProps)(DetailsPage);
