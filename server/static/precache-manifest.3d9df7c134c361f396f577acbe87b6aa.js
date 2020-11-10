ced6cc005de8fe7fb29872b2fa00bd6d5090905c
Ashween <ashweenmankash@gmail.com>
2020-11-10 13:09:55 +0530

    When react project is built, it will automatically copy it to the server's static and template folder

---
+++ server/static/precache-manifest.3d9df7c134c361f396f577acbe87b6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab60aa4313396c3e0c7c8a7575440aaa",
+    "url": "/static/index.html"
+  },
+  {
+    "revision": "755ad48f21bac08cbfdb",
+    "url": "/static/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59a77ff050389404151a",
+    "url": "/static/static/js/2.b9705390.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/static/js/2.b9705390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "755ad48f21bac08cbfdb",
+    "url": "/static/static/js/main.063e8ecc.chunk.js"
+  },
+  {
+    "revision": "67cd2147a2831ffda338",
+    "url": "/static/static/js/runtime-main.8020aa0a.js"
+  }
+]);
---
+++ server/static/precache-manifest.cf3173ded0708daaf26ba56aee7cc9ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49c3f6f6a49089b0cc29fe50dba8d31",
+    "url": "/static/react/index.html"
+  },
+  {
+    "revision": "09523c5341f86adfe5c8",
+    "url": "/static/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "59a77ff050389404151a",
+    "url": "/static/react/static/js/2.b9705390.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/react/static/js/2.b9705390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09523c5341f86adfe5c8",
+    "url": "/static/react/static/js/main.8acca625.chunk.js"
+  },
+  {
+    "revision": "f50146702588644146a0",
+    "url": "/static/react/static/js/runtime-main.d9b5c61e.js"
+  }
+]);
